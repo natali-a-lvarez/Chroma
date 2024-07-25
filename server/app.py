0bcc94c0c2feb41fb237509bb75df958c048e2ac
Tatiana <tstwrt7@gmail.com>
2024-07-25 15:05:43 -0400

    Set up project file structure

---
+++ server/app.py
@@ -0,0 +1,30 @@
+# Import Dependencies
+
+
+
+# Import Database
+
+
+
+
+
+# Create an instance of Flask
+def create_app():
+
+    # Configure the DB
+
+    # Import Routes
+
+    # Register Blueprints
+
+
+
+
+
+# Database Setup
+
+
+
+
+
+# Run the Application
