3b58c513aae692bfaceb500d8f902a8d45fe981f
Natalia Alvarez <169085366+natalia-alvarez-mck@users.noreply.github.com>
2024-07-26 17:04:05 -0400

    creating nav and global classes

--- client/react/components/App.jsx
+++ client/react/components/App.jsx
@@ -1,24 +1,20 @@
 import React, { useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import Navigation from "./Navigation";
 
 // Import Components
 import LoginButton from "./Login";
 import LogoutButton from "./Logout";
 
 function App() {
-
-
-
-
-
-    return (
-        <div>
-            <h1>Chroma</h1>
-            <LoginButton />
-            <LogoutButton />
-        </div>
-    );
-
+  return (
+    <>
+      <Navigation />
+      <h1>Chroma</h1>
+      <LoginButton />
+      <LogoutButton />
+    </>
+  );
 }
 
 export default App;
