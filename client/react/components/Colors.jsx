866f09341b2d921d04ab94e62024bf2561ba0a0e
Tatiana <tstwrt7@gmail.com>
2024-08-02 13:39:59 -0400

    Updated Saves styles and seed data

--- client/react/components/Colors.jsx
+++ client/react/components/Colors.jsx
@@ -9,11 +9,35 @@ function Colors({ userData }) {
 
     function handleCopyColor(currColor) {
         navigator.clipboard.writeText("#" + currColor);
-        alert(`Copied #${currColor} to clipboard!`);
+        // Show the message once the color is copied
+        const message = document.getElementById("message");
+        message.style.display = "block";
+
+        // Change the background color of the message button to the copied color
+        const messageButton = document.getElementById("message-button");
+        messageButton.style.backgroundColor = "#" + currColor;
+
+        // Change the text color based on the brightness of the color
+        const color = currColor;
+        const r = parseInt(color.substr(0, 2), 16);
+        const g = parseInt(color.substr(2, 2), 16);
+        const b = parseInt(color.substr(4, 2), 16);
+        const brightness = Math.sqrt((r * r * .299) + (g * g * .587) + (b * b * .114));
+        if (brightness > 130) {
+            messageButton.style.color = "black";
+        } else {
+            messageButton.style.color = "white";
+        }
+        
+        // Hide the message after 1.5 seconds
+        setTimeout(() => {
+            message.style.display = "none";
+        }, 1500);
     }
 
     return (
-        <div className="saved-colors" style={{display: userColors && userColors.length > 1 ? 'flex' : 'block', flexWrap: userColors && userColors.length > 1 ? 'wrap' : 'nowrap', justifyContent: userColors && userColors.length > 1 ? 'space-evenly' : 'center'}}>
+        <>
+        <div className="saved-colors" id={userColors && userColors.length >= 5 ? "five-saved-colors" : "less-five-saved-colors"}>
             {isAuthenticated && userColors ? (
                 userColors.map((color, idx) => (
                     <div className="saved-color-container" key={idx}>
@@ -26,6 +50,11 @@ function Colors({ userData }) {
                     <h5>No Saved Colors!</h5>
                 )}
         </div>
+
+        <div id="message" style={{display: 'none'}}>
+            <button id="message-button">Color copied to the clipboard!</button>
+        </div>
+        </>
     );
 }
 
