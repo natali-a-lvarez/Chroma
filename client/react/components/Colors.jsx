fe546f4c69b7c873b145d880c7340ff586ebacca
Natalia Alvarez <169085366+natalia-alvarez-mck@users.noreply.github.com>
2024-08-15 11:43:39 -0400

    adding copy icon to all hex codes

--- client/react/components/Colors.jsx
+++ client/react/components/Colors.jsx
@@ -1,61 +1,81 @@
 import React, { useState, useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { CopyOutline } from "react-ionicons";
 
 import "../styles/colors.css";
 
 function Colors({ userData }) {
-    const { isAuthenticated } = useAuth0();
-    const userColors = userData.saved_colors;
+  const { isAuthenticated } = useAuth0();
+  const userColors = userData.saved_colors;
 
-    function handleCopyColor(currColor) {
-        navigator.clipboard.writeText("#" + currColor);
-        // Show the message once the color is copied
-        const message = document.getElementById("message");
-        message.style.display = "block";
+  function handleCopyColor(currColor) {
+    navigator.clipboard.writeText("#" + currColor);
+    // Show the message once the color is copied
+    const message = document.getElementById("message");
+    message.style.display = "block";
 
-        // Change the background color of the message button to the copied color
-        const messageButton = document.getElementById("message-button");
-        messageButton.style.backgroundColor = "#" + currColor;
+    // Change the background color of the message button to the copied color
+    const messageButton = document.getElementById("message-button");
+    messageButton.style.backgroundColor = "#" + currColor;
 
-        // Change the text color based on the brightness of the color
-        const color = currColor;
-        const r = parseInt(color.substr(0, 2), 16);
-        const g = parseInt(color.substr(2, 2), 16);
-        const b = parseInt(color.substr(4, 2), 16);
-        const brightness = Math.sqrt((r * r * .299) + (g * g * .587) + (b * b * .114));
-        if (brightness > 130) {
-            messageButton.style.color = "black";
-        } else {
-            messageButton.style.color = "white";
-        }
-        
-        // Hide the message after 1.5 seconds
-        setTimeout(() => {
-            message.style.display = "none";
-        }, 1500);
+    // Change the text color based on the brightness of the color
+    const color = currColor;
+    const r = parseInt(color.substr(0, 2), 16);
+    const g = parseInt(color.substr(2, 2), 16);
+    const b = parseInt(color.substr(4, 2), 16);
+    const brightness = Math.sqrt(r * r * 0.299 + g * g * 0.587 + b * b * 0.114);
+    if (brightness > 130) {
+      messageButton.style.color = "black";
+    } else {
+      messageButton.style.color = "white";
     }
 
-    return (
-        <>
-        <div className="saved-colors" id={userColors && userColors.length >= 5 ? "five-saved-colors" : "less-five-saved-colors"}>
-            {isAuthenticated && userColors ? (
-                userColors.map((color, idx) => (
-                    <div className="saved-color-container" key={idx}>
-                        <div className="saved-color" style={{ backgroundColor: "#" + color }}></div>
-                        <button onClick={() => handleCopyColor(color)} className="hex-code" id={color}>
-                            #{color}
-                        </button>
-                    </div>
-                ))) : !userColors && (
-                    <h5>No Saved Colors!</h5>
-                )}
-        </div>
+    // Hide the message after 1.5 seconds
+    setTimeout(() => {
+      message.style.display = "none";
+    }, 1500);
+  }
 
-        <div id="message" style={{display: 'none'}}>
-            <button id="message-button">Color copied to the clipboard!</button>
-        </div>
-        </>
-    );
+  return (
+    <>
+      <div
+        className="saved-colors"
+        id={
+          userColors && userColors.length >= 5
+            ? "five-saved-colors"
+            : "less-five-saved-colors"
+        }
+      >
+        {isAuthenticated && userColors
+          ? userColors.map((color, idx) => (
+              <div className="saved-color-container" key={idx}>
+                <div
+                  className="saved-color"
+                  style={{ backgroundColor: "#" + color }}
+                ></div>
+                <button
+                  onClick={() => handleCopyColor(color)}
+                  className="hex-code"
+                  id={color}
+                >
+                  #{color}
+                  <CopyOutline
+                    className="copy-icon"
+                    color={"#fff"}
+                    height="18px"
+                    width="18px"
+                  />
+                </button>
+              </div>
+            ))
+          : !userColors && <h5>No Saved Colors!</h5>}
+      </div>
+
+      <div id="message" style={{ display: "none" }}>
+        <button id="message-button">Color copied to the clipboard!</button>
+      </div>
+    </>
+  );
 }
 
 export default Colors;
