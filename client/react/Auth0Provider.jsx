d6c6eb8442372cf8adff8fb38ad34417cf053e20
Natalia Alvarez <169085366+natalia-alvarez-mck@users.noreply.github.com>
2024-08-22 18:08:03 -0400

    updating redirect URI

--- client/react/Auth0Provider.jsx
+++ client/react/Auth0Provider.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Auth0Provider } from '@auth0/auth0-react';
-import App from './components/App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
+import App from "./components/App";
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 
 root.render(
-<Auth0Provider
+  <Auth0Provider
     domain={process.env.REACT_APP_AUTH0_DOMAIN}
     clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
     authorizationParams={{
-      redirect_uri: "http://localhost:1234",
+      redirect_uri: "https://chroma-client.onrender.com",
     }}
   >
     <App />
-  </Auth0Provider>,
+  </Auth0Provider>
 );
